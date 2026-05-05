45e77ce533ac575f9b5a7e5bd5ba6fc47d50ed9c
Sachin Barali <baralisachin5925@gmail.com>
2026-05-05 09:17:28 +0545

    login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { LoginSchema } from "../../schemas/auth";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(LoginSchema) });
+
+  function submitHandler(data) {
+    console.log("login daga ===> ", data);
+  }
+
   return (
     <div className="container h-100">
       <div className="d-flex justify-content-center align-items-center">
         <div className="col-md-4 shadow-sm rounded my-5 p-5">
           <h4 className="text-center">Welcome Back!</h4>
-          <form>
+          <form onSubmit={handleSubmit(submitHandler)}>
             <div class="mb-3 mt-4">
               <label for="exampleInputEmail1" className="form-label text-muted">
                 Email address
@@ -18,7 +31,13 @@ const Login = () => {
                 placeholder="something@example.com"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
+                {...register("email")}
               />
+              {errors.email && (
+                <p className="text-danger small-text mt-1">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
             <div class="mb-3 text-muted">
               <label for="exampleInputPassword1" class="form-label">
@@ -29,17 +48,26 @@ const Login = () => {
                 placeholder="Enter your password"
                 className="form-control"
                 id="exampleInputPassword1"
+                {...register("password")}
               />
+              {errors.password && (
+                <p className="text-danger small-text mt-1">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
             <div class="d-grid">
-              <button class="btn btn-primary" type="button">
+              <button class="btn btn-primary" type="submit">
                 Login
               </button>
             </div>
           </form>
           <p className="small-text mt-3 text-center">
             Don't have an account?{" "}
-            <Link to={"/signup"} className="text-decoration-none text-primary pointer">
+            <Link
+              to={"/signup"}
+              className="text-decoration-none text-primary pointer"
+            >
               Sign up
             </Link>
           </p>
--- frontend/src/schemas/auth.js
+++ frontend/src/schemas/auth.js
@@ -1,21 +1,33 @@
 import * as yup from "yup";
 
+const emailValidation = yup
+  .string()
+  .required("Email is required.")
+  .matches(
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    "Invalid email format.",
+  );
+
+const passwordValidation = yup
+  .string()
+  .required("Password is required")
+  .min(8, "Password must be at least 8 character");
+
 export const SignUpSchema = yup.object({
   username: yup
     .string()
     .required("Username is required")
     .max(10, "Username must be at most 10 characters")
     .min(3, "Username must be at least 3 characters"),
-  email: yup
-    .string()
-    .required("Email is required.")
-    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
-  password: yup
-    .string()
-    .required("Password is required")
-    .min(8, "Password must be at least 8 character"),
+  email: emailValidation,
+  password: passwordValidation,
   confirmPassword: yup
     .string()
     .oneOf([yup.ref("password")], "Confirm password must be same as password")
     .required("Confirm password is required."),
+});
+
+export const LoginSchema = yup.object({
+  email: emailValidation,
+  password: passwordValidation,
 });
