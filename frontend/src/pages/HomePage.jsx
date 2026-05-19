2be3f068051084ea43c89f82ca1067ef737d79f0
Sachin Barali <baralisachin5925@gmail.com>
2026-05-19 09:33:17 +0545

    token saved

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import { Link } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  return <>
-  <h1>Homepage</h1>
-    <Link to={'/about'}>About</Link>
-  </>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login");
+    }
+  },[navigate]);
+  return (
+    <>
+      <h1>Homepage</h1>
+      <Link to={"/about"}>About</Link>
+    </>
+  );
 };
 
 export default HomePage;
